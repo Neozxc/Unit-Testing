0ed10ec15d3eb84d9ddafdf89cc08f1c1c889383
Lapinas <mrlapinas99@gmail.com>
2022-01-14 15:55:58 +0000

    sending to github

---
+++ app.js
@@ -0,0 +1,16 @@
+// const add = (num1, num2) => {
+//     return num1 + num2;
+// };
+
+// let myArray = ["Dan", "Stuart", "Ben", "Nedas"];
+
+// module.exports = add;
+
+const something = (name, age) => {
+    objectives = [
+        "name",
+        "age"
+    ]
+};
+
+module.exports = something;
---
+++ tests/app.test.js
@@ -0,0 +1,72 @@
+// const add = require("../app");
+// const myArray = require("../app");
+// const something = require("../app");
+
+
+// test("should contain Nedas in myArray", () => {
+//     expect(myArray).toContain("Nedas");
+// });
+
+// it("adds 5 + 5 to equal 10", () => {
+//     expect(add(5, 5)).toBe(10);
+// });
+
+// const status = [
+//     'name',
+//     'age',
+//   ];
+  
+// test('status has name and age on it', () => {
+//     expect(status).toContain('name', 'age');
+//   });
+
+
+
+
+
+
+
+
+
+
+
+
+// CHALLENGES
+
+// Null
+// const bloop = () => {
+//     return null;
+// }
+
+// test("bloop returns null", () => {
+//     expect(bloop()).toBeNull();
+// });
+
+// const films = {
+//     spiderman: true,
+//     superman: false
+// };
+
+// Truthy
+
+// test("spiderman returns true", () => {
+//     expect(films.spiderman).toBeTruthy();
+// });
+
+// Falsy
+
+// test("superman returns false", () => {
+//     expect(films.superman).toBeFalsy();
+// });
+
+// Function with 2 properties
+
+// test('the number of elements must match exactly', () => {
+//       expect([{name: 'Nedas'}, {age: 21}]).toMatchObject([{name: 'Nedas'}, {age: 21}]);
+// });
+
+// Array containing 6 or more
+
+// it("only has 6 letters", () => {
+//     expect("adcdef").toHaveLength(6);
+// })
